a32b2f4ec0129df38baa1e8f8676b3616b2d2d9a
Tudor <7089284+tudddorrr@users.noreply.github.com>
2023-05-02 00:29:47 +0100

    d3 sign

---
+++ scripts/quests/sign_d3_quest.gd
@@ -0,0 +1,8 @@
+extends QuestData
+class_name SignD3Quest
+
+func _get_text() -> String:
+	return "NORTH: THE VILLAGE\nEAST: CLARKE'S FARM\nSOUTH: THE COVE\nWEST: THE DARK FOREST"
+
+func _get_options() -> Array[Dictionary]:
+	return []
