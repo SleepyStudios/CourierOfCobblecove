586b8f21570ad1d007b6cb5ed8a01d1c9a85acdb
tudor <7089284+tudddorrr@users.noreply.github.com>
2023-05-01 02:43:05 +0100

    add new npcs and sprites

---
+++ scripts/zone_name_progress_label.gd
@@ -0,0 +1,32 @@
+extends Label
+
+var text_to_use: String
+
+func _ready():
+	Global.scene_transition.on_scene_changed.connect(_on_scene_changed)
+
+func _on_scene_changed(new_scene_path: String):
+	var regex = RegEx.new()
+	regex.compile("(?<=zones\\/)[a-z]\\d(?=\\.tscn)")
+	var current_zone = regex.search(new_scene_path).get_string()
+	
+	var zone_name = "Name not set :("
+	match current_zone:
+		"b4": zone_name = "THE CLEARING"
+		"c4": zone_name = "THE FOREST"
+		"c5": zone_name = "THE MOUNTAIN"
+		"c6": zone_name = "EAST BEACH"
+		"d3": zone_name = "NORTH CROSSROADS"
+		"d4": zone_name = "SOUTH CROSSROADS"
+		"d5": zone_name = "THE BOUNDARY"
+		"d6": zone_name = "WEST BEACH"
+		"e3": zone_name = "CLARKE'S FARM"
+		"e4": zone_name = "THE CASTLE APPROACH"
+		"e5": zone_name = "THE SNAKEPIT"
+		"f4": zone_name = "CASTLE GATE"
+	
+	var deliveries = len(Global.quest_manager.completed_quests)
+	text_to_use = "%s\n%s DELIVERIES" % [zone_name, deliveries]
+	
+func _process(delta):
+	text = text_to_use
