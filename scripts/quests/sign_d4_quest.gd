90b7bf2ff3ebe76826fd19ddab29bfbda26459d5
Tudor <7089284+tudddorrr@users.noreply.github.com>
2023-05-01 21:45:49 +0100

    beach -> cove

--- scripts/quests/sign_d4_quest.gd
+++ scripts/quests/sign_d4_quest.gd
@@ -2,7 +2,7 @@ extends QuestData
 class_name SignD4Quest
 
 func _get_text() -> String:
-	return "NORTH: THE VILLAGE\nEAST: THE CASTLE\nSOUTH: THE BEACH\nWEST: THE FOREST"
+	return "NORTH: THE VILLAGE\nEAST: THE CASTLE\nSOUTH: THE COVE\nWEST: THE FOREST"
 
 func _get_options() -> Array[Dictionary]:
 	return []
